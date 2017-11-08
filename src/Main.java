2f12733fadef644767589c217bd6f8b27e452d38
cristi97 <cristianscurtu@yahoo.com>
2017-11-08 16:54:19 +0200

    Player Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Player player=new Player();
+        player.name="Cristi";
+        player.health=20;
+        player.weapon="Tomahawk";
+        int damage=10;
+        player.loseHealth(damage);
+        System.out.println(player.healthRemaining());
+        damage=11;
+        player.loseHealth(damage);
+        System.out.println(player.healthRemaining());
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    public String name,weapon;
+    public int health;
+    public void loseHealth(int damage){
+        this.health=this.health-damage;
+        if(this.health<=0)
+            System.out.println("Player fainted");
+    }
+    public int healthRemaining(){
+        return this.health;
+    }
+}
